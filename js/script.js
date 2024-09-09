823748ba543a75266cb09fff106cece4cd72aaea
Raushan Gupta <raushangupta1425@gmail.com>
2024-09-09 21:31:25 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log("Welcome");
+
+     window.onscroll = function () {
+         scrollFunction();
+     };
+
+     let lastScrollTop = 0;
+
+     function scrollFunction() {
+         let currentScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+         if (currentScrollTop > lastScrollTop) {
+             // Scroll down - hide the navbar
+             document.getElementById("navbar").classList.remove("show-navbar1");
+             document.getElementById("navbar").classList.remove("home-show");
+             document.getElementById("navbar").classList.add("hide-navbar1");
+         }
+         else if (currentScrollTop === 0) {
+             // Ensure navbar is always visible when at the topmost view (before any scroll)
+             document.getElementById("navbar").classList.remove("hide-navbar1");
+             document.getElementById("navbar").classList.remove("show-navbar1");
+             document.getElementById("navbar").classList.add("home-show");
+         }
+         else {
+             // Scroll up - show the navbar
+             document.getElementById("navbar").classList.remove("home-show");
+             document.getElementById("navbar").classList.remove("hide-navbar1");
+             document.getElementById("navbar").classList.add("show-navbar1");
+         }
+
+         lastScrollTop = currentScrollTop;
+     }
+
+
+
